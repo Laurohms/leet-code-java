9a7d68fd41e5236268c39074de4d545899e1bb3a
Laurohms <laurohmsantos@gmail.com>
2024-09-07 18:08:43 -0300

    merge sorted array

--- src/main/java/challenges/Solution.java
+++ src/main/java/challenges/Solution.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
         if (n >= 0) System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
     }
