9ac8b629bbb8f12406d192236ce4ad6cac3af680
Laurohms <laurohmsantos@gmail.com>
2024-09-07 18:06:48 -0300

    merge sorted array

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+import challenges.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/challenges/Solution.java
@@ -0,0 +1,11 @@
+package challenges;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n >= 0) System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+}
